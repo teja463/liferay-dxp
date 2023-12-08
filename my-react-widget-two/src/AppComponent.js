217b42616975f45522147fe00d080639a7f43ef8
teja463 <teja463@gmail.com>
2023-12-08 10:49:30 +0530

    added another react widget

---
+++ my-react-widget-two/src/AppComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class extends React.Component {
+	render() {
+		return (
+            <div>
+				<h1>React Widget 2!</h1>
+				<div>
+        	        <span className="tag">Portlet Namespace:</span> 
+					<span className="value">{this.props.portletNamespace}</span>
+				</div>
+				<div>
+    	            <span className="tag">Context Path:</span>
+					<span className="value">{this.props.contextPath}</span>
+				</div>
+				<div>
+	                <span className="tag">Portlet Element Id:</span>
+					<span className="value">{this.props.portletElementId}</span>
+				</div>
+				
+				<div>
+					<span className="tag">Configuration:</span>
+					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
+				</div>
+				
+			</div>
+		);
+	}	
+}
